8f7975117e1b68b1954750d52942b13131c63a90
uniterriziki <mpekethuuniter@gmail.com>
2024-02-08 09:56:17 +0300

    commit all

--- 7kyu/js/printer_error.js
+++ 7kyu/js/printer_error.js
@@ -1,6 +1,8 @@
-// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, 
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to
+// use colors which, 
 //for the sake of simplicity, are named with letters from a to m.
-// The colors used by the printer are recorded in a control string. For example a "good" control string 
+// The colors used by the printer are recorded in a control string. For example a "good" 
+//control string 
 //would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time 
 //color h then one time color a...
 // Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced 
